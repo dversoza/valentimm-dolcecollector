5b3576307a3626c168960f2607a7078a015870d1
Matheus Valentim <meduardo.valentim@gmail.com>
2023-08-03 11:30:02 -0300

    feat: created first spider(dolcespider) to collect all coffees data

--- dolcescraper/spiders/dolcespider.py
+++ dolcescraper/spiders/dolcespider.py
@@ -7,4 +7,14 @@ class DolcespiderSpider(scrapy.Spider):
     start_urls = ["https://www.nescafe-dolcegusto.com.br/sabores"]
 
     def parse(self, response):
-        pass
+        coffees = response.css(".product-card__content")
+        for coffee in coffees:
+            yield {
+                "name": coffee.css(".product-card__name a ::text").get(),
+                "price": coffee.css(".price-wrapper span ::text").get(),
+                "capsules": coffee.css(".product-card__name a ::attr(href)").get(),
+                "link": coffee.css("a::attr(href)").get(),
+                "points": coffee.css(".product-card__price .price_pp ::text").get(),
+                "description": coffee.css(".product-card__description--short ::text").get(),
+            }
+
